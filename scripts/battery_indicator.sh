d74a42ee6937b82fe63774df54aa156b4bf62bfd
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2015-09-19 11:20:44 -0300

    add scripts and tmux configuration

---
+++ scripts/battery_indicator.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# modified from http://ficate.com/blog/2012/10/15/battery-life-in-the-land-of-tmux/
+
+HEART=' ♥'
+
+battery_info=`ioreg -rc AppleSmartBattery`
+current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
+total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+
+charged_slots=$(echo "((($current_charge/$total_charge)*10)/10)+1" | bc -l | cut -d '.' -f 1)
+if [[ $charged_slots -gt 10 ]]; then
+  charged_slots=10
+fi
+
+echo -n '#[fg=colour196]'
+for i in `seq 1 $charged_slots`; do echo -n "$HEART"; done
+
+if [[ $charged_slots -lt 10 ]]; then
+  echo -n '#[fg=colour254]'
+  for i in `seq 1 $(echo "10-$charged_slots" | bc)`; do echo -n "$HEART"; done
+fi
